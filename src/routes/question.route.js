aa7eb37c42040f4f13e12699b0bb0f6daa91a7f5
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-19 15:40:24 +0200

    User and Question routes

---
+++ src/routes/question.route.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const questionController = require("../controllers/question.controller.js");
+const auth = require("../middleware/auth.middleware");
+const Role = require("../utils/userRoles.utils");
+const awaitHandlerFactory = require("../middleware/awaitHandlerFactory.middleware");
+const {
+  createDefaultQuestionSchema,
+} = require("../middleware/validators/defaultQuestionValidator.middleware.js");
+
+router.post(
+  "/add-default-question",
+  auth(),
+  createDefaultQuestionSchema,
+  awaitHandlerFactory(questionController.addQuestions)
+);
+
+module.exports = router;
