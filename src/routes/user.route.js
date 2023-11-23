1ac8fb30639304b5852fee25c35b4d891df502d8
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-24 00:50:29 +0200

    Update user.route.js

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -11,12 +11,10 @@ const {
   validateLogin,
 } = require("../middleware/validators/userValidator.middleware");
 
-router.get("/", auth(), awaitHandlerFactory(userController.getAllUsers)); // localhost:3000/api/v1/users
-router.get("/id/:id", auth(), awaitHandlerFactory(userController.getUserById)); // localhost:3000/api/v1/users/id/1
 router.get(
   "/username/:username",
   auth(),
-  awaitHandlerFactory(userController.getUserByuserName)
+  awaitHandlerFactory(userController.getUserByUserName)
 ); // localhost:3000/api/v1/users/usersname/julia
 router.get(
   "/whoami",
