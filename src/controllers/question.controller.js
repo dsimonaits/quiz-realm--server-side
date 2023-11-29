d47bda03ac05336aed766c17cd5d0f6fb4285a04
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-29 23:00:13 +0200

    Update question.controller.js

--- src/controllers/question.controller.js
+++ src/controllers/question.controller.js
@@ -2,9 +2,10 @@ const defaultQuestionModel = require("../models/defaultQuestion.model");
 const HttpException = require("../utils/HttpException.utils");
 const { validationResult } = require("express-validator");
 const dotenv = require("dotenv");
+const userQuestionModel = require("../models/userQuestion.model");
 dotenv.config();
 
-class DefaultQuestionController {
+class questionController {
   getQuestions = async (req, res, next) => {
     this.checkValidation(req);
 
@@ -36,6 +37,40 @@ class DefaultQuestionController {
     }
   };
 
+  addUserQuestions = async (req, res, next) => {
+    this.checkValidation(req);
+    try {
+      const questions = req.body;
+      const id = req.params.id;
+
+      const newQuestions = await userQuestionModel.insertUserQuestions(
+        questions,
+        id
+      );
+
+      res.json({
+        code: 200,
+        message: "Questions added successfully",
+        data: { questionID: newQuestions },
+      });
+    } catch (error) {
+      console.error(error);
+      next(new HttpException(500, "Internal Server Error"));
+    }
+  };
+
+  getUserQuestions = async (req, res, next) => {
+    this.checkValidation(req);
+    const id = req.params.id;
+
+    const questions = await userQuestionModel.getUserQuestions(id);
+
+    res.status(201).json({
+      code: 201,
+      questions: questions,
+    });
+  };
+
   checkValidation = (req) => {
     const request = Array.isArray(req.body)
       ? req.body.map((question) => {
@@ -51,4 +86,4 @@ class DefaultQuestionController {
   };
 }
 
-module.exports = new DefaultQuestionController();
+module.exports = new questionController();
